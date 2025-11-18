989c77e887d9ae9167c3fa892dd52a32aaa7a94c
alpha-code02 <bhemharanghay@gmail.com>
2025-11-18 21:24:25 +0400

    done21

---
+++ Exercise 03 - Biography/ex3.py
@@ -0,0 +1,11 @@
+def exercise_3():
+    name = "Ethan"
+    hometown = "Dubai"
+    age = 18
+
+    biography = {"name": name, "hometown": hometown, "age": age}
+
+    print(biography["name"], biography["hometown"], biography["age"], sep="\n")
+
+
+exercise_3()
