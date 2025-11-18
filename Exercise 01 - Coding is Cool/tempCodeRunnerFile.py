f673024e92cea3b1ad7ddef1cc4fb0793ff4b949
alpha-code02 <bhemharanghay@gmail.com>
2025-11-18 21:22:28 +0400

    done

---
+++ Exercise 01 - Coding is Cool/tempCodeRunnerFile.py
@@ -0,0 +1,15 @@
+def exercise_2():
+    a = 8
+    b = 10
+    total = a + b
+    print("Exercise 2 - Sum of", a, "and", b, "is", total)
+
+
+def exercise_3():
+    name = "Ethan"
+    hometown = "Dubai"
+    age = 18
+
+    biography = {"name": name, "hometown": hometown, "age": age}
+
+    print(biography["name"], biography["hometown"], biography["age"], sep="\n")
