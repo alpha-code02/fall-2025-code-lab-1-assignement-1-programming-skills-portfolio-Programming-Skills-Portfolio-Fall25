91d2819b27d2f43cd1c1493332507dddf0f6a4f3
alpha-code02 <bhemharanghay@gmail.com>
2025-11-19 14:47:00 +0400

    Assessment-1

--- Exercise 01 - Coding is Cool/code1.py
+++
@@ -1,9 +0,0 @@
-def exercise_1():
-    word1 = "Coding"
-    word2 = " is "
-    word3 = "Cool"
-
-    print(word1 + word2 + word3)
-
-
-exercise_1()
--- Exercise 02- Simple Sums/ex2.py
+++
@@ -1,8 +0,0 @@
-def exercise_2():
-    a = 8
-    b = 10
-    total = a + b
-    print("Exercise 2 - Sum of", a, "and", b, "is", total)
-
-
-exercise_2()
